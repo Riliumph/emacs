8811fa1de24b0bf10b6dcb8402c54effec503db4
Rily <rilumph@yahoo.co.jp>
2017-04-12 23:56:39 +0900

    [Update] Change display string

--- inits/51_user_interface.el
+++ inits/51_user_interface.el
@@ -1,8 +1,10 @@
+;;表示系の設定
 ;; タイトルバーにファイルのフルパス表示
 (setq frame-title-format
 	  (format "%%f - Emacs@%s" (system-name)))
 ;;(menu-bar-mode -1)  ;; メニューバー非表示
 (tool-bar-mode 0)     ;; ツールバー非表示
+(fset 'yes-or-no-p 'y-or-n-p)      ;; "yes or no" -> "y or n"
 
 ;; 行数設定
 (cond
