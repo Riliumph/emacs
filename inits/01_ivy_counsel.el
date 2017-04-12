b3fac18ea700428f693e096dc2dd808c872f5f7c
Rily <rilumph@yahoo.co.jp>
2017-04-13 03:03:02 +0900

    [Update] search command
    When calling swiper during selection,
    treat the selected string as a swiper's query

--- inits/01_ivy_counsel.el
+++ inits/01_ivy_counsel.el
@@ -1,3 +1,11 @@
 (require 'ivy)
 (ivy-mode 1)
-(global-set-key "\C-s" 'swiper)
+
+(global-set-key (kbd "C-s") (lambda ()
+			      (interactive)
+			      (swiper (cond
+				       ;; If there is selection use it
+				       ((and transient-mark-mode mark-active (not (eq (mark) (point))))
+					(buffer-substring-no-properties (mark) (point)))
+				       ;; Otherwise, use symbol at point or empty
+				       (t (format "%s" ""))))))
